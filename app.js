94edd0a43e512865eef96376b64365753932ceee
CRHarding <casey.r.harding@gmail.com>
2018-06-28 10:11:47 -0400

    deploying

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 require('dotenv').config();
 
